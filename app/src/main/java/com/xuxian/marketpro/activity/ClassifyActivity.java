f2925eb8b2412f5c670f73d7446d44e3c67054d9
lubo7525274 <lubo_wen@126.com>
2016-08-14 10:03:33 +0800

    1.17.2 classifyactivity

--- app/src/main/java/com/xuxian/marketpro/activity/ClassifyActivity.java
+++ app/src/main/java/com/xuxian/marketpro/activity/ClassifyActivity.java
@@ -1,13 +1,39 @@
 package com.xuxian.marketpro.activity;
 
 import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ListView;
 
+import com.xuxian.marketpro.R;
 import com.xuxian.marketpro.activity.supers.SuperSherlockActivity;
+import com.xuxian.marketpro.presentation.View.adapter.ClassifyAdapter;
+import com.xuxian.marketpro.presentation.View.widght.ActivityStateView;
+import com.xuxian.marketpro.presentation.entity.ClassifyEntity;
+
+import java.util.List;
 
 /**
  * Created by youarenotin on 16/8/14.
  */
 public class ClassifyActivity extends SuperSherlockActivity{
+    private NetworkAsyncTask calssifyAsyncTask;
+    private ActivityStateView emptyview_state;
+    private ClassifyAdapter mAdapter;
+    private ListView mClassifyList;
+    private List<ClassifyEntity> mDataList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.classify_content);
+        initTitleBar();
+        initFindViewById();
+        setListener();
+        init();
+    }
+
     @Override
     protected Activity getActivity() {
         return this;
@@ -20,7 +46,7 @@ public class ClassifyActivity extends SuperSherlockActivity{
 
     @Override
     protected void initTitleBar() {
-
+        setTitle("分类");
     }
 
     @Override
@@ -31,5 +57,12 @@ public class ClassifyActivity extends SuperSherlockActivity{
     @Override
     protected void setListener() {
 
+    }
+
+    private class NetworkAsyncTask extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] params) {
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/com/xuxian/marketpro/presentation/entity/ClassifyEntity.java
@@ -0,0 +1,34 @@
+package com.xuxian.marketpro.presentation.entity;
+
+/**
+ * Created by youarenotin on 16/8/14.
+ */
+public class ClassifyEntity {
+    private String classifyid;
+    private String classifyname;
+    private String icon;
+
+    public String getClassifyid() {
+        return classifyid;
+    }
+
+    public void setClassifyid(String classifyid) {
+        this.classifyid = classifyid;
+    }
+
+    public String getClassifyname() {
+        return classifyname;
+    }
+
+    public void setClassifyname(String classifyname) {
+        this.classifyname = classifyname;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+}
