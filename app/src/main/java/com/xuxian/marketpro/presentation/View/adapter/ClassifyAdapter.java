b298a56a26b702f63c03777d402f7fa01559ae0b
lubo7525274 <lubo_wen@126.com>
2016-08-14 10:07:16 +0800

    1.17.3 classify adapter

--- app/src/main/java/com/xuxian/marketpro/presentation/View/adapter/ClassifyAdapter.java
+++ app/src/main/java/com/xuxian/marketpro/presentation/View/adapter/ClassifyAdapter.java
@@ -1,30 +1,50 @@
 package com.xuxian.marketpro.presentation.View.adapter;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.xuxian.marketpro.presentation.entity.ClassifyEntity;
+
+import java.util.List;
+
 /**
  * Created by youarenotin on 16/8/14.
  */
-public class ClassifyAdapter extends BaseAdapter{
+public class ClassifyAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<ClassifyEntity> mDataList;
+
+    public ClassifyAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public void setData(List<ClassifyEntity> data){
+        this.mDataList=data;
+        notifyDataSetChanged();
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        if (mDataList == null || mDataList.isEmpty())
+            return 0;
+        return mDataList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mDataList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
         return null;
     }
 }
