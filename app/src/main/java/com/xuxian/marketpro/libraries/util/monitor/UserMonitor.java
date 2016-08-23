f2f184f91ec3a08f3be068e467ed339955d269a3
lubo7525274 <lubo_wen@126.com>
2016-08-23 12:24:45 +0800

    1.22.3 monitor

--- app/src/main/java/com/xuxian/marketpro/libraries/util/monitor/UserMonitor.java
+++ app/src/main/java/com/xuxian/marketpro/libraries/util/monitor/UserMonitor.java
@@ -1,7 +1,25 @@
 package com.xuxian.marketpro.libraries.util.monitor;
 
+import com.xuxian.marketpro.presentation.entity.UserEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by youarenotin on 16/8/23.
  */
 public class UserMonitor {
+    private static UserMonitor monitor;
+    private Map<String,UserMonitorCallback> maps = new HashMap<>();
+
+    public interface UserMonitorCallback{
+        void appOperation(UserEntity userEntity);
+    }
+
+    public static UserMonitor getInstance(){
+        if (monitor==null){
+            monitor=new UserMonitor();
+        }
+        return monitor;
+    }
 }
