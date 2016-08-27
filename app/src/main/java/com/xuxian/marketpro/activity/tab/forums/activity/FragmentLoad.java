1696de2209f03c6954fe0a4d9a9286839c6a41d2
lubo7525274 <lubo_wen@126.com>
2016-08-27 20:20:36 +0800

    1.27.6

--- app/src/main/java/com/xuxian/marketpro/activity/tab/forums/activity/FragmentLoad.java
+++ app/src/main/java/com/xuxian/marketpro/activity/tab/forums/activity/FragmentLoad.java
@@ -1,17 +1,27 @@
 package com.xuxian.marketpro.activity.tab.forums.activity;
 
 import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.ab.util.AbTokenUtil;
+import com.xuxian.marketpro.R;
 import com.xuxian.marketpro.activity.supers.SuperFragment;
+import com.xuxian.marketpro.libraries.util.Tools;
+import com.xuxian.marketpro.presentation.View.refreshlayout.XuXianNormalRefreshViewHolder;
 import com.xuxian.marketpro.presentation.View.refreshlayout.XuXianRefreshLayout;
 import com.xuxian.marketpro.presentation.View.viewpage.BannerViewPager;
 import com.xuxian.marketpro.presentation.View.widght.ActivityStateView;
 import com.xuxian.marketpro.presentation.db.UserDb;
+import com.xuxian.marketpro.presentation.entity.ForumListEntity;
+import com.xuxian.marketpro.presentation.entity.ForumsEntity;
 import com.xuxian.marketpro.presentation.entity.UserEntity;
+
+import java.util.List;
 
 import static com.xuxian.marketpro.activity.tab.forums.activity.ForumListActivity.*;
 
@@ -39,6 +49,34 @@ public class FragmentLoad extends SuperFragment implements XuXianRefreshLayout.X
 
     @Override
     protected void init() {
+        this.lv_forum_list.setVisibility(8);
+        this.forumsListAdapter = new ForumsListAdapter(getActivity());
+        this.lv_forum_list.setAdapter(this.forumsListAdapter);
+        this.userDb = new UserDb(getActivity());
+        this.userDto = this.userDb.queryData();
+        if (this.userDto != null) {
+            this.user_id = this.userDto.getUserid();
+        }
+        this.forumsViewPagerAdapter.setUserDto(this.userDto);
+        setLoadingStatus(Tools.TYPE_IS_FROM_NET);
+        this.forumListAsyncTask = new NetworkAsyncTask( null,getActivity());
+        this.forumListAsyncTask.execute(this.fid, this.currentPage, AbTokenUtil.getToken(this.user_id));
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view_layout = inflater.inflate(R.layout.activity_forum_list_layout, null);
+        initTitleBar();
+        initFindViewById(view_layout);
+        initViewPage();
+        setListener();
+        init();
+        return view_layout;
+    }
+
+    private void initViewPage() {
 
     }
 
@@ -49,25 +87,42 @@ public class FragmentLoad extends SuperFragment implements XuXianRefreshLayout.X
 
     @Override
     protected void initFindViewById(View view) {
-
+        this.lv_forum_list = (ListView) view.findViewById(R.id.lv_forum_list);
+        this.bla_forum_list = (XuXianRefreshLayout) view.findViewById(R.id.bla_forum_list);
+        this.emptyview_state = (ActivityStateView) view.findViewById(R.id.emptyview_state);
+        this.emptyview_state.setVisibility(View.VISIBLE);
+        this.emptyview_state.setState(ActivityStateView.ACTIVITY_STATE_LOADING);
     }
 
     @Override
     protected void setListener() {
-
+        this.bla_forum_list.setDelegate(this);
+        this.bla_forum_list.setRefreshViewHolder(new XuXianNormalRefreshViewHolder(getActivity(), true));
+        ForumListMonitor.getInstance().register(new ForumListMonitorCallback() {
+            public void AppOperation(boolean isforumList) {
+                if (FragmentLoad.this.userDto != null) {
+                    FragmentLoad.this.setLoadingStatus(Tools.TYPE_IS_FROM_PULL);
+                    FragmentLoad.this.forumListAsyncTask = new NetworkAsyncTask(FragmentLoad.this.getActivity(), null);
+                    FragmentLoad.this.forumListAsyncTask.execute(new Object[]{FragmentLoad.this.fid, Integer.valueOf(FragmentLoad.this.currentPage), AbTokenUtil.getToken(FragmentLoad.this.userDto.getUserid())});
+                }
+            }
+        }, ForumListActivity.class.getName());
     }
 
     @Override
     public void onXuXianRefreshLayoutBeginRefreshing(XuXianRefreshLayout refreshLayout) {
-
+        setLoadingStatus(Tools.TYPE_IS_FROM_PULL);
+        currentPage=1;
+        forumListAsyncTask = new NetworkAsyncTask(null,getActivity());
+        forumListAsyncTask.execute(new Object[]{fid,currentPage,AbTokenUtil.getToken(user_id)});
     }
 
     @Override
     public boolean onXuXianRefreshLayoutBeginLoadingMore(XuXianRefreshLayout refreshLayout) {
         this.currentPage++;
         if (this.currentPage<=this.totalpage){//如果当前页不大于等于总页数
-            setLoadingStatus(222);
-            forumListAsyncTask = new ForumListActivity.NetworkAsyncTask(null,getActivity());
+            setLoadingStatus(Tools.TYPE_IS_FROM_PULLUP);
+            forumListAsyncTask = new NetworkAsyncTask(null,getActivity());
             forumListAsyncTask.execute(new Object[]{fid,currentPage, AbTokenUtil.getToken(user_id)});
             return true;
         }
@@ -76,8 +131,22 @@ public class FragmentLoad extends SuperFragment implements XuXianRefreshLayout.X
         return false;
     }
 
+
+
     private void endLoad() {
         this.bla_forum_list.endLoadingMore();
         this.bla_forum_list.endRefreshing();
+    }
+
+    public List<ForumListEntity> setForumList(List<ForumListEntity> data){
+        int length =data.size();
+        String temp="";
+        int i =0 ;
+        while(i<length-1 && data.get(i).getTopped().equals("1") && !data.get(i).getTid().equals(temp)){
+            data.get(i).setType(1);
+            temp=data.get(i).getTid();
+            i++;
+        }
+        return  data;
     }
 }
---
+++ app/src/main/java/com/xuxian/marketpro/presentation/entity/ForumListEntity.java
@@ -0,0 +1,336 @@
+package com.xuxian.marketpro.presentation.entity;
+
+/**
+ * Created by youarenotin on 16/8/27.
+ */
+public class ForumListEntity extends BaseEntity {
+    private static final long serialVersionUID = 1;
+    private String content;
+    private String created_ip;
+    private String created_time;
+    private String created_userid;
+    private String created_username;
+    private String digest;
+    private String disabled;
+    private String fid;
+    private String highlight;
+    private String hits;
+    private String icon;
+    private String ifshield;
+    private String ifupload;
+    private String inspect;
+    private String ischeck;
+    private long lastpost_time;
+    private String lastpost_userid;
+    private String lastpost_username;
+    private String like_count;
+    private String modified_ip;
+    private String modified_time;
+    private String modified_userid;
+    private String modified_username;
+    private String overtime;
+    private String replies;
+    private String reply_notice;
+    private String reply_topped;
+    private String special;
+    private String special_sort;
+    private String subject;
+    private String thread_status;
+    private String tid;
+    private Integer topic_type;
+    private String topped;
+    private String tpcstatus;
+    private int type;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreated_ip() {
+        return created_ip;
+    }
+
+    public void setCreated_ip(String created_ip) {
+        this.created_ip = created_ip;
+    }
+
+    public String getCreated_time() {
+        return created_time;
+    }
+
+    public void setCreated_time(String created_time) {
+        this.created_time = created_time;
+    }
+
+    public String getCreated_userid() {
+        return created_userid;
+    }
+
+    public void setCreated_userid(String created_userid) {
+        this.created_userid = created_userid;
+    }
+
+    public String getCreated_username() {
+        return created_username;
+    }
+
+    public void setCreated_username(String created_username) {
+        this.created_username = created_username;
+    }
+
+    public String getDigest() {
+        return digest;
+    }
+
+    public void setDigest(String digest) {
+        this.digest = digest;
+    }
+
+    public String getDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(String disabled) {
+        this.disabled = disabled;
+    }
+
+    public String getFid() {
+        return fid;
+    }
+
+    public void setFid(String fid) {
+        this.fid = fid;
+    }
+
+    public String getHighlight() {
+        return highlight;
+    }
+
+    public void setHighlight(String highlight) {
+        this.highlight = highlight;
+    }
+
+    public String getHits() {
+        return hits;
+    }
+
+    public void setHits(String hits) {
+        this.hits = hits;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getIfshield() {
+        return ifshield;
+    }
+
+    public void setIfshield(String ifshield) {
+        this.ifshield = ifshield;
+    }
+
+    public String getIfupload() {
+        return ifupload;
+    }
+
+    public void setIfupload(String ifupload) {
+        this.ifupload = ifupload;
+    }
+
+    public String getInspect() {
+        return inspect;
+    }
+
+    public void setInspect(String inspect) {
+        this.inspect = inspect;
+    }
+
+    public String getIscheck() {
+        return ischeck;
+    }
+
+    public void setIscheck(String ischeck) {
+        this.ischeck = ischeck;
+    }
+
+    public long getLastpost_time() {
+        return lastpost_time;
+    }
+
+    public void setLastpost_time(long lastpost_time) {
+        this.lastpost_time = lastpost_time;
+    }
+
+    public String getLastpost_userid() {
+        return lastpost_userid;
+    }
+
+    public void setLastpost_userid(String lastpost_userid) {
+        this.lastpost_userid = lastpost_userid;
+    }
+
+    public String getLastpost_username() {
+        return lastpost_username;
+    }
+
+    public void setLastpost_username(String lastpost_username) {
+        this.lastpost_username = lastpost_username;
+    }
+
+    public String getLike_count() {
+        return like_count;
+    }
+
+    public void setLike_count(String like_count) {
+        this.like_count = like_count;
+    }
+
+    public String getModified_ip() {
+        return modified_ip;
+    }
+
+    public void setModified_ip(String modified_ip) {
+        this.modified_ip = modified_ip;
+    }
+
+    public String getModified_time() {
+        return modified_time;
+    }
+
+    public void setModified_time(String modified_time) {
+        this.modified_time = modified_time;
+    }
+
+    public String getModified_userid() {
+        return modified_userid;
+    }
+
+    public void setModified_userid(String modified_userid) {
+        this.modified_userid = modified_userid;
+    }
+
+    public String getModified_username() {
+        return modified_username;
+    }
+
+    public void setModified_username(String modified_username) {
+        this.modified_username = modified_username;
+    }
+
+    public String getOvertime() {
+        return overtime;
+    }
+
+    public void setOvertime(String overtime) {
+        this.overtime = overtime;
+    }
+
+    public String getReplies() {
+        return replies;
+    }
+
+    public void setReplies(String replies) {
+        this.replies = replies;
+    }
+
+    public String getReply_notice() {
+        return reply_notice;
+    }
+
+    public void setReply_notice(String reply_notice) {
+        this.reply_notice = reply_notice;
+    }
+
+    public String getReply_topped() {
+        return reply_topped;
+    }
+
+    public void setReply_topped(String reply_topped) {
+        this.reply_topped = reply_topped;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getSpecial() {
+        return special;
+    }
+
+    public void setSpecial(String special) {
+        this.special = special;
+    }
+
+    public String getSpecial_sort() {
+        return special_sort;
+    }
+
+    public void setSpecial_sort(String special_sort) {
+        this.special_sort = special_sort;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getThread_status() {
+        return thread_status;
+    }
+
+    public void setThread_status(String thread_status) {
+        this.thread_status = thread_status;
+    }
+
+    public String getTid() {
+        return tid;
+    }
+
+    public void setTid(String tid) {
+        this.tid = tid;
+    }
+
+    public Integer getTopic_type() {
+        return topic_type;
+    }
+
+    public void setTopic_type(Integer topic_type) {
+        this.topic_type = topic_type;
+    }
+
+    public String getTopped() {
+        return topped;
+    }
+
+    public void setTopped(String topped) {
+        this.topped = topped;
+    }
+
+    public String getTpcstatus() {
+        return tpcstatus;
+    }
+
+    public void setTpcstatus(String tpcstatus) {
+        this.tpcstatus = tpcstatus;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
